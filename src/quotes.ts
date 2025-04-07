ad14799fa0cd2190099adf192c6540044328076e
Dan Lichtin <lichtindan@gmail.com>
2025-04-06 23:44:47 -0400

    Adding more quotes

--- src/quotes.ts
+++ src/quotes.ts
@@ -3,21 +3,42 @@ export const quotes = [
     text: "Connect. Connect",
     author: "Luke Belmar",
     bg: "bg-quote-luke",
-    //bg: "bg-purple-600",
+    didTheySayIt: true,
   },
   {
     text: "If not we are gonna have to bounce brother",
     author: "Nate Belmar",
     bg: "bg-quote-luke",
+    didTheySayIt: true,
   },
   {
     text: "What color is your Bugatti?",
     author: "Andrew Tate",
     bg: "bg-quote-luke",
+    didTheySayIt: true,
   },
   {
     text: "Steve",
     author: "Andrew Tate",
-    bg: "bg-quote-luke"
+    bg: "bg-quote-luke",
+    didTheySayIt: true,
+  },
+  {
+    text: "Everybody worships but nobody worships the hard work",
+    author: "Luke Belmar",
+    bg: "bg-quote-luke",
+    didTheySayIt: true,
+  },
+  {
+    text: "Guys, this is the video game, we are in it",
+    author: "Luke Belmar",
+    bg: "bg-quote-luke",
+    didTheySayIt: true,
+  },
+  {
+    text: "If you guys work for me, six pack or your fired",
+    author: "Andy Elliott",
+    bg: "bg-quote-luke",
+    didTheySayIt: true,
   },
 ]
